f229512ebcdbb691cca37b39b4a8162936d3c33b
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-01-22 17:52:48 +0100

    adds routing POC

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const generatePublic=require('./generators/generatePublic')
 const {generateSrc}=require('./generators/generateSrc')
 const Scenario=require('./virtual/Scenario')
+const {Route}=require('./virtual/view')
 
 
 generatePublic.generateHtmlRoot('frontend', 'this is a frontend')
@@ -10,12 +11,35 @@ const s = new Scenario()
 
 const {index, app} = generateSrc(s)
 
-const test1=s.createComponent('test1', {type:'component', dependencies:[]})
-const test2=s.createComponent('test2', {type:'component', dependencies:[]})
-const test3=s.createComponent('test3', {type:'component', dependencies:[]})
+const test1=s.createComponent('test1', {type:'genericComponent', dependencies:[]})
+const test2=s.createComponent('test2', {type:'genericComponent', dependencies:[]})
+const test3=s.createComponent('test3', {type:'genericComponent', dependencies:[]})
 
 s.addLocalDependency(test1, app)
+//
+//////////////////////////////////////////////////////
 //how do you kill local dependencies ?
+//s.removeNode('app') OR s.removeNode(app.getNodeId())
+//_________________________________________________
+
+////////////////////////////////////////////////////
+//test : Routes handling
+
+//<Route path='/' element={<Test1/>}/>
+const rootRoute= new Route(test1)
+
+//<Route path='about' element={<Test2/>}/>
+const levelOneChild=new Route(test2, 'about')
+
+app.addTree(rootRoute)
+app.addChildRoute(rootRoute, levelOneChild)
+
+const sibling = new Route(test3, '/otherRouter')
+app.addTree(sibling)
+
+app.logTrees()
+app.generateJsxRouting()
+
 
 
 
--- virtual/view.js
+++ virtual/view.js
@@ -92,15 +92,99 @@ class rootComponent extends component{
 }
 
 class appComponent extends component{
+	trees={}
 	constructor(name,options){
 		super(name,options)
 		this.location='root'
 		this.type='applicationComponent'
+	}
+	addTree(newRoot){
+		this.trees[newRoot.getName()]=newRoot
+	}
+	addChildRoute(oneRoot, newBorn){
+		this.trees[oneRoot.getName()].addChild(newBorn)
+	}
+	logTrees(){
+		console.log(this.trees)
+	}
+	generateJsxRouting(){
+		for(const [key,value] of Object.entries(this.trees)){
+			value.doRecursivePresentation(0)
+		}
+	}
+	//add routes
+	//pass a component and an url
+	//addRoute('root', child){
+	//routes.push({location:root, childRoutes:none, display:child, innerAccess:child.nodeId()})
+	//addRoute('child', anotherChild){
+	//routes.push({location:child, 
+	//'child' has to be interpreted as : '**parentName**'
+	//each route should be an object because it allows for another easy tracking of the 
+	//structure of the Router
+	//you build a tree 
+	//and when the tree is built you can call the method to rewrite the file in order to 
+	//update it
+	//then the tree should be saved as Routes in appComponent and can be reused to generate a 
+	//navBar
+	//})
+	//}
+	//}
+}
+
+class Route{
+	childs={}
+	component
+	name
+	location
+	//<Route path='/' element={<NodeName/>}>
+	//
+	//     <Route path='about' element={<LevelOneChild/>}>
+	//         <Route path='this' element={<LevelTwoChild/>}/>
+	//     </Route>
+	//
+	//     <Route path='contact' element={<Sibling/>}>
+	//</Route>
+	//
+	//root= new Route{location='root', component='NodeName'}
+	//child = new Route{location='about' component='LevelOneChild'}
+	//IMPORTANT:
+	//appComponent.createRoot(root)
+	//appComponent.addChild(root, child)
+	//root{...root, childs : {LevelOneChild: {child{location:'about', component:'Levelxone'}}}}
+	//
+	constructor(component,location){
+		this.name=component.getNodeId()
+		this.component=component
+		this.location=location?location:'/'
+	}
+
+	addChild(child){
+		this.childs[child.getName()]=child
+	}
+
+	getName(){
+		return this.name
+	}
+
+	doRecursivePresentation(level){
+		console.log(`${this.generateTabs(level)}---<Route path='${this.location}' element={<${this.component.getNodeId()}/>}/>`)
+			for(const [key,value] of Object.entries(this.childs)){
+				value.doRecursivePresentation(level+1)
+			}
+	}
+
+	generateTabs(times){
+		let result =`` 
+		for(let i = 0; i < times; i++){
+			result=result.concat('-----')
+		}
+		return result
 	}
 }
 
 module.exports={
 	component,
 	appComponent,
-	rootComponent
+	rootComponent,
+	Route,
 }
